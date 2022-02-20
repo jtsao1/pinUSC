e41a4e89c02e187bf31c52bf60b686015c2166c2
Jady <hsichint@usc.edu>
2022-02-20 00:28:53 -0800

    leaflet layer 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,100 @@
 // alert('ahh');
-var map = L.map('map').setView([34.02210372599966, -118.28610378009327], 15.5);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var uscCoord = [34.02210372599966, -118.28610378009327]
+
+// function readSingleFile(f) {
+//     var file = f.target.files[0];
+//     if (!file) {
+//         return;
+//     }
+//     var reader = new FileReader();
+//     reader.onload = function(f) {
+//         var contents = f.target.result;
+//         displayContents(contents);
+//     };
+//     reader.readAsText(file);
+//     return reader;
+// }
+// function displayContents(contents) {
+//     console.log(contents);
+//     var markers = JSON.parse(contents);
+//    var element = document.getElementById('file-content');
+//    element.textContent = contents;
+// }
+
+// document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+
+// var result = JSON.parse()
+
+// let reader = new FileReader();
+// reader.readAsText("input.json");
+// reader.onload = function() {
+//     console.log(reader.result);
+//     };
+result = [
+  {
+    "name": "Jady's Tree",
+    "latitudes": 34.019878651034475,
+    "longitudes": -118.2857264632366,
+    "img": "https://panoraven.com/en/embed/BsTi9klO2P",
+    "stars": 4.5,
+    "reviewCNT": 3,
+  },
+  {
+    "name": "Tommy Trojan",
+    "latitudes": 34.020578599590166,
+    "longitudes": -118.28544484137373,
+    "img": "https://panoraven.com/en/embed/ezeIVALRKS",
+    "stars": 3.5,
+    "reviewCNT": 300,
+  },
+  {
+    "name": "Alumni Park",
+    "latitudes": 34.02075754352828,
+    "longitudes": -118.28446270718727,
+    "img": "https://panoraven.com/en/embed/7wlBDxWAXd",
+    "stars": 4,
+    "reviewCNT": 54,
+  },
+  {
+    "name": "Trojan Family Room",
+    "latitudes": 34.01992933219847,
+    "longitudes": -118.28622627524467,
+    "img": "https://panoraven.com/en/embed/nIIeMJnTQf",
+    "stars": 4.5,
+    "reviewCNT": 3,
+  },
+]
+
+var lg = L.layerGroup()
+
+for (let i=0; i<result.length; i++) {
+    var name = result[i].name;
+    var stars = result[i].stars;
+    var reviewCNT = result[i].reviewCNT;
+    var img = result[i].img;
+    var popUpStr = `<b>${name}</b><br>${stars} stars ${reviewCNT} reviews<br><iframe width=\"300px\" height=\"300px\" allowFullScreen=\"true\" allow=\"accelerometer; magnetometer; gyroscope\" style=\"display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);\" src=\"${img}\"></iframe>`;
+    var mark = L.marker([result[i].latitudes, result[i].longitudes]).bindPopup(popUpStr).addTo(lg);
+}
+
+var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiamFkeXQiLCJhIjoiY2t6dXBkdWc5MDNtejJ1bXk5MTRsMHVtZSJ9.MfvsNyJvrt1Kl24zD3wuNA'
-}).addTo(map);
+});
 
-var jadys_tree = L.marker([34.019878651034475, -118.2857264632366]).addTo(map)
-jadys_tree.bindPopup("<b>Jady's Tree!</b><br><iframe width=\"300px\" height=\"300px\" allowFullScreen=\"true\" allow=\"accelerometer; magnetometer; gyroscope\" style=\"display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);\" src=\"https://panoraven.com/en/embed/BsTi9klO2P\"></iframe>");
-var tommy_trojan = L.marker([34.020578599590166, -118.28544484137373]).addTo(map)
-var alumni_park = L.marker([34.02075754352828, -118.28446270718727]).addTo(map)
-var trojan_family_room = L.marker([34.01992933219847, -118.28622627524467]).addTo(map)
-trojan_family_room.bindPopup("<b>Trojan Family Room</b><br><iframe width=\"300px\" height=\"300px\" allowFullScreen=\"true\" allow=\"accelerometer; magnetometer; gyroscope\" style=\"display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);\" src=\"https://panoraven.com/en/embed/nIIeMJnTQf\"></iframe>")
+var map = L.map('map', {
+    center: uscCoord,
+    zoom: 15,
+    layers: [streets, lg]
+});
 
-// <iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/en/embed/BsTi9klO2P"></iframe>
-// <iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/en/embed/nIIeMJnTQf"></iframe>
+var baseMaps = {
+    "Streets": streets
+}
+var overlayMaps = {
+    "Eat": lg
+};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
