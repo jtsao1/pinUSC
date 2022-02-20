bd457ec85ea82069ad9c85faaa06f3cfcdabe950
Jady <hsichint@usc.edu>
2022-02-20 12:14:20 -0800

    Front End Glow-up

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+import pyodbc
 from datetime import datetime
 # from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
-import pyodbc
 server = 'my-usc-server.database.windows.net'
 database = 'my-database'
 username = 'adminserver'
-password = 'myuscserver1!'   
-driver= '{ODBC Driver 17 for SQL Server}'
+password = 'myuscserver1!'
+driver = '{ODBC Driver 17 for SQL Server}'
 
 
 @app.route("/", methods=['GET'])
@@ -17,40 +17,45 @@ def main():
     return render_template("index.html")
 
 # @app.route("/profile/<companyName>", methods=['GET'])
+
+
 @app.route('/query/<queryCols>', methods=['GET'])
 def queryDB(queryCols):
-   print('enter queryDB')
-   # indoor or outdoor
-   has_or_rule = 'indoor' in queryCols and 'outdoor' in queryCols
-   queryColElems = queryCols.split(' ')
+    print('enter queryDB')
+    # indoor or outdoor
+    has_or_rule = 'indoor' in queryCols and 'outdoor' in queryCols
+    queryColElems = queryCols.split(' ')
 
-   whereRule = ''
-   for queryCol in queryColElems:
-      if has_or_rule and (queryCol=='indoor' or queryCol=='outdoor'): continue
-      whereRule = f"{queryCol}='true'" if not whereRule else f"{whereRule} and {queryCol}='true'"   
-      
-   if has_or_rule:
-      # not only indoor and outdoor rule
-      if len(queryColElems)!=2: 
-         whereRule += ' and'
-      whereRule += " NOT (indoor='false' and outdoor='false')"
+    whereRule = ''
+    for queryCol in queryColElems:
+        if has_or_rule and (queryCol == 'indoor' or queryCol == 'outdoor'):
+            continue
+        whereRule = f"{queryCol}='true'" if not whereRule else f"{whereRule} and {queryCol}='true'"
 
-   queryText = f"SELECT * FROM location where {whereRule}"
-   print(f'queryText = {queryText}')
+    if has_or_rule:
+        # not only indoor and outdoor rule
+        if len(queryColElems) != 2:
+            whereRule += ' and'
+        whereRule += " NOT (indoor='false' and outdoor='false')"
 
-   with pyodbc.connect(f'DRIVER={driver};SERVER=tcp:{server};PORT=5000;DATABASE={database};UID={username};PWD={password}') as conn:
-      with conn.cursor() as cursor:
-         # cursor.execute("""SELECT * FROM location""")
-         cursor.execute(queryText)
-         # row = cursor.fetchone()
-         # while row:
-         #       print (str(row[0]) + " " + str(row[1]))
-         #       row = cursor.fetchone()
+    queryText = f"SELECT * FROM location where {whereRule}"
+    print(f'queryText = {queryText}')
 
-         queryResult = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-         print(queryResult)
-         return jsonify(queryResult)
-               
+    # with pyodbc.connect(f'DRIVER={driver};SERVER=tcp:{server};PORT=5000;DATABASE={database};UID={username};PWD={password}') as conn:
+    with f'Server=tcp:my-usc-server.database.windows.net,1433;Initial Catalog=my-database;Persist Security Info=False;User ID=adminserver;Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' as conn:
+        with conn.cursor() as cursor:
+            # cursor.execute("""SELECT * FROM location""")
+            cursor.execute(queryText)
+            # row = cursor.fetchone()
+            # while row:
+            #       print (str(row[0]) + " " + str(row[1]))
+            #       row = cursor.fetchone()
+
+            queryResult = [dict(zip([column[0] for column in cursor.description], row))
+                           for row in cursor.fetchall()]
+            print(queryResult)
+            return jsonify(queryResult)
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
